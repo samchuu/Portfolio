6048cce53dac09cd7b7d66f0ffe1e4e66a1c1ecd
Sam Chu <samchupiano@gmail.com>
2021-08-12 13:22:00 +0200

    nav bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import sam from "../icons/sam.jpeg";
 import facebook from "../icons/facebook.png";
 import instagram from "../icons/instagram.png";
 import linkedin from "../icons/linkedin.png";
 function Home() {
   const [isHamburgerActive, setIsHamburgerActive] = useState(false);
-
+  const [isScrollActive, setIsScrollActive] = useState(false);
   const hamburgerHandler = () => {
     setIsHamburgerActive(!isHamburgerActive);
   };
+
   //nav-bar smooth scroll handlers
   const homeHandler = () => {
     const homePage = document.querySelector(".home");
+    setIsHamburgerActive(false);
+
     homePage.scrollIntoView({ behavior: "smooth" });
   };
   const aboutHandler = () => {
     const aboutPage = document.querySelector(".about");
     setIsHamburgerActive(false);
-
+    setIsScrollActive(true);
     aboutPage.scrollIntoView({ behavior: "smooth" });
   };
   const workHandler = () => {
     const workPage = document.querySelector(".work-scroll");
     workPage.scrollIntoView({ behavior: "smooth" });
     setIsHamburgerActive(false);
+    setIsScrollActive(true);
   };
 
-  const contactHandler = () => {
-    const contactPage = document.querySelector(".footer-icon-box");
-    contactPage.scrollIntoView({ behavior: "smooth" });
-    setIsHamburgerActive(false);
-  };
+  // const contactHandler = () => {
+  //   const contactPage = document.querySelector(".footer-icon-box");
+  //   contactPage.scrollIntoView({ behavior: "smooth" });
+  //   setIsHamburgerActive(false);
+  //   setIsScrollActive(true);
+  // };
 
   const closeNavHandler = () => {
     setIsHamburgerActive(false);
   };
+
+  //show hamburger when user is on top of the page
+  // window.addEventListener("scroll", function () {
+  //   if (window.scrollY == 0) {
+  //     setIsScrollActive(false);
+  //   } else {
+  //     setIsScrollActive(true);
+  //   }
+  // });
   return (
     <div className="home">
       <header>
@@ -68,7 +82,7 @@ function Home() {
               <div className="nav-text" href="/" onClick={workHandler}>
                 Work
               </div>
-              <div className="nav-text" href="/" onClick={contactHandler}>
+              <div className="nav-text" href="/">
                 Resume
               </div>
             </div>
